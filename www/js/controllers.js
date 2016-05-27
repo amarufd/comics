868ffbbbffa3e1b8f18dbda4c114bb22a76c895d
Amaru <amarud@gmail.com>
2016-05-26 21:51:51 -0400

    mostrando comics v2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,31 +24,19 @@ angular.module('starter.controllers', [])
                                    },function(e){
                                    $scope.alert = e;})
             
-            $scope.getTheComics=function(id){
-                apiMV.getTheComics(id).then(function(d){
-                      console.log(d.data.results);
-                      $rootScope.chat = d.data.results[0];
-                      },function(e){
-                      $scope.alert = e;})
-            }
-
-  /*$scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    apiMV.remove(chat);
-  };*/
+            
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, apiMV, $timeout, $rootScope) {
+.controller('ChatDetailCtrl', function($scope, $stateParams, apiMV, $timeout) {
             
-            $scope.chat = $rootScope.chat;
-            console.log($scope.chat);
-            
-                     apiMV.getTheComics($stateParams.comicsId).then(function(d){
-                                        console.log(d.data.results);
-                                                                    $rootScope.chat = d.data.results[0];
+            apiMV.getTheComics($stateParams.comicsId).then(function(d){
+                                        $scope.chat = d.data.results[0];
                                 },function(e){
                                         $scope.alert = e;})
-                     $timeout(function(){console.log($rootScope.chat.title)},500)
+            
+            console.log($scope.chat);
+            $timeout(function(){console.log($scope.chat)},500)
+            
             
             
             
